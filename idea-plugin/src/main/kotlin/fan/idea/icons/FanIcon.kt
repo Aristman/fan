9050e139b2011b4d0785c1e23f9f8ee748c73c7a
Sergey Aristov <Saristov@it-one.ru>
2026-04-26 11:11:39 +0300

    fix: tool window button not appearing — FanIcon implements Icon, add resource-bundle
    Root cause: plugin.xml icon="fan.idea.icons.FanIcon" referenced a Kotlin
    object that didn't implement Icon, so IntelliJ couldn't render the button.
    
    Fix:
    - FanIcon.kt: implement Icon via interface delegation
    - plugin.xml: add <resource-bundle>messages.FanBundle</resource-bundle>
    - Clear sandbox to remove stale workspace state

--- idea-plugin/src/main/kotlin/fan/idea/icons/FanIcon.kt
+++ idea-plugin/src/main/kotlin/fan/idea/icons/FanIcon.kt
@@ -3,6 +3,4 @@ package fan.idea.icons
 import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
-object FanIcon {
-    val TOOL_WINDOW: Icon = IconLoader.getIcon("/icons/fan_icon.svg", FanIcon::class.java)
-}
+object FanIcon : Icon by IconLoader.getIcon("/icons/fan_icon.svg", FanIcon::class.java)
